6ec991a7e412492ff1fadf70e2ecf75777238187
tomoyanakano <tomo1999724@gmail.com>
2023-12-08 12:17:18 +0900

    :tada: Initial commit

---
+++ wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+
+-- colors
+config.color_scheme = "nord"
+config.window_background_opacity = 0.93
+
+-- font
+config.font = wezterm.font("Firge35Nerd Console")
+config.font_size = 13.0
+
+-- and finally, return the configuration to wezterm
+return config
